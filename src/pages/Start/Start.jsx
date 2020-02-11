394b401ed5c13f03c60e8b58bbff7b265500e332
Alessandro Di Tecco <alessandro@ditecco.me>
2020-02-11 01:28:34 +0100

    Created basic scaffolding for auth page

--- src/pages/Start/Start.jsx
+++ src/pages/Start/Start.jsx
@@ -8,10 +8,32 @@ import { log, storage } from "../../utils";
 import { API_KEY } from "../../constants";
 import Modal from "../../components/Modal/Modal";
 
-export default function Start() {
-  const [keyIsPresent, setKeyIsPresent] = useState(false);
+import * as firebase from "firebase/app";
+import "firebase/auth";
+import firebaseConfig from "../../config/firebaseConfig";
 
-  useEffect(() => checkApiKey(), []);
+export default function Start() {
+  const [isAuthorized, setIsAuthorized] = useState(false);
+  const [keyIsPresent, setKeyIsPresent] = useState(false);
+  const [showModal, setShowModal] = useState(false);
+
+  useEffect(() => {
+    // checkApiKey()
+  }, []);
+
+  function userSignup({ email, password }) {
+    firebase
+      .auth()
+      .createUserWithEmailAndPassword(email, password)
+      .catch(function(error) {
+        // Handle Errors here.
+        var errorCode = error.code;
+        var errorMessage = error.message;
+        // ...
+      });
+  }
+
+  function userLogin(params) {}
 
   /**
    * Checks for the required API key
@@ -37,11 +59,42 @@ export default function Start() {
 
   return (
     <>
-      <Modal />
-      <div className="Start">
-        <div>Signup or login, Insert API key (WIP)</div>
+      <Modal open={showModal} closeAction={() => setShowModal(false)}>
+        <h3>Signup!</h3>
 
-        {keyIsPresent && <Link to="/home">Home</Link>}
+        <form action="" className="signupForm">
+          <input type="text" className="signupEmail" placeholder="email" />
+          <input
+            type="password"
+            className="signupPassword"
+            placeholder="password"
+          />
+
+          <button
+            type="button"
+            className="signupButton"
+            onClick={e => null}
+            style={{ color: "white" }} // remove
+          >
+            Signup
+          </button>
+        </form>
+      </Modal>
+
+      <div className="Start">
+        <h3>Signup or login</h3>
+        <button
+          type="button"
+          className="signupButton"
+          onClick={() => setShowModal(true)}
+        >
+          Signup
+        </button>
+        <button type="button" className="signupButton">
+          Login
+        </button>
+
+        {isAuthorized && keyIsPresent && <Link to="/home">Home</Link>}
       </div>
     </>
   );
