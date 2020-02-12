f8a9afe8f4c1e5b6f75feab0f782d9edb8fb9c91
Alessandro Di Tecco <alessandro@ditecco.me>
2020-02-12 01:31:42 +0100

    Created more logic & UI for signup/login flow

--- src/pages/Start/Start.jsx
+++ src/pages/Start/Start.jsx
@@ -2,7 +2,7 @@
 Start
 --------------------------------- */
 
-import React, { useEffect, useState } from "react";
+import React, { useEffect, useState, useReducer } from "react";
 import { Link } from "react-router-dom";
 import { log, storage } from "../../utils";
 import { API_KEY } from "../../constants";
@@ -13,27 +13,88 @@ import "firebase/auth";
 import firebaseConfig from "../../config/firebaseConfig";
 
 export default function Start() {
-  const [isAuthorized, setIsAuthorized] = useState(false);
-  const [keyIsPresent, setKeyIsPresent] = useState(false);
-  const [showModal, setShowModal] = useState(false);
+  const [state, setState] = useReducer(
+    (state, newState) => ({ ...state, ...newState }),
+    {
+      signingUp: false,
+      loggingIn: false,
+      isAuthorized: false,
+      keyIsPresent: false,
+      showModal: false,
+      hasError: { error: false, errorMeta: {} },
+      email: "",
+      password: ""
+    }
+  );
+
+  const {
+    isAuthorized,
+    keyIsPresent,
+    showModal,
+    email,
+    password,
+    signingUp,
+    loggingIn
+  } = state;
+
+  // const [isAuthorized, setIsAuthorized] = useState(false);
+  // const [keyIsPresent, setKeyIsPresent] = useState(false);
+  // const [showModal, setShowModal] = useState(false);
 
   useEffect(() => {
     // checkApiKey()
   }, []);
 
-  function userSignup({ email, password }) {
+  /**
+   * xyz
+   */
+
+  function handleSignup(credentials) {
+    const { email, password } = credentials;
+
+    if (!email || !password) {
+      window.alert("nope!");
+      return;
+    }
+
+    // provide some form of validation
+
     firebase
       .auth()
       .createUserWithEmailAndPassword(email, password)
-      .catch(function(error) {
-        // Handle Errors here.
-        var errorCode = error.code;
-        var errorMessage = error.message;
-        // ...
+      .then(({ users }) => {
+        const [newUser] = users;
+
+        log("new signup!");
+
+        setState({ email: "", password: "" });
+      })
+      .catch(error => {
+        const { code, message } = error;
+        setState({ hasError: { error: true, errorMeta: { code, message } } });
       });
   }
 
-  function userLogin(params) {}
+  /**
+   * xyz
+   */
+
+  function handleLogin(credentials) {
+    const { email, password } = credentials;
+
+    firebase
+      .auth()
+      .signInWithEmailAndPassword(email, password)
+      .then(response => {
+        log("new login");
+
+        setState({ isAuthorized: true, showModal: false, loggingIn: false });
+      })
+      .catch(error => {
+        const { code, message } = error;
+        setState({ hasError: { error: true, errorMeta: { code, message } } });
+      });
+  }
 
   /**
    * Checks for the required API key
@@ -54,30 +115,73 @@ export default function Start() {
       storage.push(storageID, requestKey);
     }
 
-    setKeyIsPresent(true);
+    setState({ keyIsPresent: true });
   }
 
   return (
     <>
-      <Modal open={showModal} closeAction={() => setShowModal(false)}>
-        <h3>Signup!</h3>
+      <Modal
+        open={showModal}
+        closeAction={() =>
+          setState({ showModal: false, signingUp: false, loggingIn: false })
+        }
+      >
+        <h3>{`${signingUp ? "Signup!" : "Login"}`}</h3>
 
-        <form action="" className="signupForm">
-          <input type="text" className="signupEmail" placeholder="email" />
-          <input
-            type="password"
-            className="signupPassword"
-            placeholder="password"
-          />
+        <form action="" className={`${signingUp ? "signupForm" : "loginForm"}`}>
+          {signingUp ? (
+            <>
+              {/* signup */}
+              <input
+                type="text"
+                className="signupEmail"
+                placeholder="email"
+                onChange={e => setState({ email: e.currentTarget.value })}
+              />
 
-          <button
-            type="button"
-            className="signupButton"
-            onClick={e => null}
-            style={{ color: "white" }} // remove
-          >
-            Signup
-          </button>
+              <input
+                type="password"
+                className="signupPassword"
+                placeholder="password"
+                onChange={e => setState({ password: e.currentTarget.value })}
+              />
+
+              <button
+                type="button"
+                className="signupButton"
+                onClick={() => handleSignup({ email, password })}
+                style={{ color: "white" }} // remove
+              >
+                Signup
+              </button>
+            </>
+          ) : (
+            <>
+              {/* login */}
+              <input
+                type="text"
+                className="loginEmail"
+                placeholder="email"
+                onChange={e => setState({ email: e.currentTarget.value })}
+              />
+
+              <input
+                type="password"
+                className="loginPassword"
+                placeholder="password"
+                onChange={e => setState({ password: e.currentTarget.value })}
+              />
+
+              <button
+                type="button"
+                className="loginButton"
+                onClick={() => handleLogin({ email, password })}
+                style={{ color: "white" }} // remove
+              >
+                Login
+              </button>
+            </>
+          )}
         </form>
       </Modal>
 
@@ -86,11 +190,15 @@ export default function Start() {
         <button
           type="button"
           className="signupButton"
-          onClick={() => setShowModal(true)}
+          onClick={() => setState({ showModal: true, signingUp: true })}
         >
           Signup
         </button>
-        <button type="button" className="signupButton">
+        <button
+          type="button"
+          className="loginButton"
+          onClick={() => setState({ showModal: true, loggingIn: true })}
+        >
           Login
         </button>
 
