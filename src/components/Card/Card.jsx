b305d837e56380454a0b5027fa63cb8a8beef379
Alessandro Di Tecco <alessandro@ditecco.me>
2020-03-22 16:59:38 +0100

    Fixed dependency issues

--- src/components/Card/Card.jsx
+++ src/components/Card/Card.jsx
@@ -2,9 +2,10 @@
 Card
 --------------------------------- */
 
-import React from "react";
+import React, { useState } from "react";
 import { log } from "../../utils";
 import MaterialIcon from "../Misc/MaterialIcon";
+import { useSpring, animated } from "react-spring";
 
 export default function Card({
   image,
@@ -17,72 +18,83 @@ export default function Card({
 }) {
   // log(arguments);
   const isWatched = false;
+  const [flipped, toggleFlipped] = useState(false);
+
+  const { transform, opacity } = useSpring({
+    opacity: flipped ? 1 : 0,
+    transform: `perspective(600px) rotateY(${flipped ? 180 : 0}deg)`,
+    config: { mass: 5, tension: 500, friction: 80 }
+  });
 
   return (
-    <a href={null} className="CardAnchor">
-      <article className="Card">
-        <div className="CardFlipControls">
-          <button onClick={e => log("d")}>
-            <MaterialIcon icon="info" />
-          </button>
-        </div>
-
-        <section className="CardMedia">
-          <div className="CardPosterCurtain">
-            <h3 className="CardPosterCurtainTitle">{title}</h3>
-
-            <ul className="CardMeta">
-              <li className="CardMetaItem">
-                <h6 className="CardMetaItemTitle">Year</h6>
-
-                {year}
-              </li>
-              <li className="CardMetaItem">
-                <h6 className="CardMetaItemTitle">Type</h6>
-
-                {type}
-              </li>
-            </ul>
+    <div className="CardContainer" onClick={() => toggleFlipped(!flipped)}>
+      <animated.div class="front">
+        <article className="Card">
+          <div className="CardFlipControls">
+            <button onClick={e => log("d")}>
+              <MaterialIcon icon="info" />
+            </button>
           </div>
 
-          <img src={image} alt={title} className="CardPoster" />
-        </section>
+          <section className="CardMedia">
+            <div className="CardPosterCurtain">
+              <h3 className="CardPosterCurtainTitle">{title}</h3>
 
-        {/* <section className="CardBody">
+              <ul className="CardMeta">
+                <li className="CardMetaItem">
+                  <h6 className="CardMetaItemTitle">Year</h6>
+
+                  {year}
+                </li>
+                <li className="CardMetaItem">
+                  <h6 className="CardMetaItemTitle">Type</h6>
+
+                  {type}
+                </li>
+              </ul>
+            </div>
+
+            <img src={image} alt={title} className="CardPoster" />
+          </section>
+
+          {/* <section className="CardBody">
           <span className="CardContent">{year + ", " + type}</span>
         </section> */}
 
-        <footer className="CardFooter">
-          <div className="CardControls">
-            {/* 
+          <footer className="CardFooter">
+            <div className="CardControls">
+              {/* 
             TODO
 
             Define how CardControls will change
             when the item is set as watched
             */}
 
-            {!isWatched ? (
-              <>
-                <button className="CardControlsButton" type="button">
-                  To Watch
-                </button>
+              {!isWatched ? (
+                <>
+                  <button className="CardControlsButton" type="button">
+                    To Watch
+                  </button>
 
-                <button
-                  className="CardControlsButton"
-                  type="button"
-                  onClick={e => {
-                    e.preventDefault();
+                  <button
+                    className="CardControlsButton"
+                    type="button"
+                    onClick={e => {
+                      e.preventDefault();
 
-                    onWatchedClick({ image, title, type, year });
-                  }}
-                >
-                  Watched <i className="material-icons">check_circle</i>
-                </button>
-              </>
-            ) : null}
-          </div>
-        </footer>
-      </article>
-    </a>
+                      onWatchedClick({ image, title, type, year });
+                    }}
+                  >
+                    Watched <i className="material-icons">check_circle</i>
+                  </button>
+                </>
+              ) : null}
+            </div>
+          </footer>
+        </article>
+      </animated.div>
+
+      <animated.div class="back">back!</animated.div>
+    </div>
   );
 }
