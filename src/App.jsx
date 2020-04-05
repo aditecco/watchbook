40680c8b484fc787b85f7bdf8c9ce9ee7d1624f0
Alessandro Di Tecco <alessandro@ditecco.me>
2020-04-05 02:09:46 +0200

    Moved Firebase config to .env

--- src/App.jsx
+++ src/App.jsx
@@ -7,7 +7,6 @@ import { storage } from "./utils";
 import * as firebase from "firebase/app";
 import "firebase/database";
 import "firebase/auth";
-import firebaseConfig from "./config/firebaseConfig";
 import initialState from "./initialState";
 import reducer from "./reducer";
 import Watched from "./pages/Watched/Watched";
@@ -32,6 +31,16 @@ import Spinner from "./components/Spinner/Spinner";
 window.storage = storage;
 
 // firebase initializers
+const firebaseConfig = {
+  apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
+  authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
+  databaseURL: process.env.REACT_APP_FIREBASE_DATABASE_URL,
+  projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.REACT_APP_FIREBASE_APP_ID
+};
+
 firebase.initializeApp(firebaseConfig);
 export const db = firebase.database();
 
