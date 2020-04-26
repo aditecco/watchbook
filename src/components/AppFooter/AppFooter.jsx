72015e4b6d294e456ca4218a151be580e6fa4965
Alessandro Di Tecco <alessandro@ditecco.me>
2020-04-26 17:16:30 +0200

    Small fixes

--- src/components/AppFooter/AppFooter.jsx
+++ src/components/AppFooter/AppFooter.jsx
@@ -8,8 +8,8 @@ export default function AppFooter() {
   return (
     <footer className="AppFooter">
       <span>
-        {/* BUILD_ID is resolved by Netlify */}
         {process.env.REACT_APP_APP_VERSION}{" "}
+        {/* BUILD_ID is resolved by Netlify */}
         {process.env.BUILD_ID && process.env.BUILD_ID.substring(0, 4)}
       </span>
     </footer>
