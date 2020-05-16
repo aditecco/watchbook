b73b8ace8b9abdbdb66332747b870b05338fcc2b
Alessandro Di Tecco <alessandro@ditecco.me>
2020-05-16 18:14:42 +0200

    Small fixes, draft stories

---
+++ src/stories/Autosuggest.stories.js
@@ -0,0 +1,14 @@
+/* ---------------------------------
+AutoSuggest
+--------------------------------- */
+
+// import React from "react";
+// import AutoSuggest from "../components/AutoSuggest/AutoSuggest";
+// import "../styles/index.scss";
+
+// export default {
+//   title: "AutoSuggest",
+//   component: AutoSuggest,
+// };
+
+// export const Default = () => <AutoSuggest />;
---
+++ src/stories/Card.stories.js
@@ -0,0 +1,14 @@
+/* ---------------------------------
+Card
+--------------------------------- */
+
+// import React from "react";
+// import Card from "../components/Card/Card";
+// import "../styles/index.scss";
+
+// export default {
+//   title: "Card",
+//   component: Card,
+// };
+
+// export const Default = () => <Card />;
