f6e3f6fb66b10a57da716c6f7b95a0f26133e86a
Alessandro Di Tecco <alessandro@ditecco.me>
2020-05-16 16:01:55 +0200

    Created test stories

--- src/stories/SearchField.stories.js
+++ src/stories/SearchField.stories.js
@@ -1,19 +1,22 @@
+/* ---------------------------------
+SearchField
+--------------------------------- */
+
 import React from "react";
 import { action } from "@storybook/addon-actions";
 import SearchField from "../components/SearchField/SearchField";
-import "../components/SearchField/build.css";
-import "../components/SearchField/SearchField.scss";
-
-export default {
-  title: "SearchField",
-  component: SearchField
-};
+import "../styles/index.scss";
 
 const SearchFieldProps = {
   searchHandler: action("handleSearch"),
   focusHandler: action("handleFocus"),
   resetHandler: action("handleSearchReset"),
-  searchQuery: ""
+  searchQuery: "",
 };
 
 export const Default = () => <SearchField {...SearchFieldProps} />;
+
+export default {
+  title: "SearchField",
+  component: SearchField,
+};
---
+++ src/stories/Spinner.stories.js
@@ -0,0 +1,14 @@
+/* ---------------------------------
+Spinner
+--------------------------------- */
+
+import React from "react";
+import Spinner from "../components/Spinner/Spinner";
+import "../styles/index.scss";
+
+export const Default = () => <Spinner />;
+
+export default {
+  title: "Spinner",
+  component: Spinner,
+};
