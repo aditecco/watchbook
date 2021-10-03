a19c60539e021f9aa887da95e18869c28b563866
Alessandro Di Tecco <alessandro@ditecco.me>
2021-10-03 23:41:58 +0200

    feature/search_by_ID | Refactored to enable alternative search params, added WIP UI

--- src/components/SearchField/SearchField.tsx
+++ src/components/SearchField/SearchField.tsx
@@ -2,7 +2,7 @@
 SearchField
 --------------------------------- */
 
-import React, { ReactElement } from "react";
+import React, { ReactElement, useState } from "react";
 import MaterialIcon from "../Misc/MaterialIcon";
 
 // TODO
@@ -23,20 +23,46 @@ export default function SearchField({
   searchHandler,
   searchQuery,
 }: OwnProps): ReactElement {
+  const [isFocused, setIsFocused] = useState<boolean>(false);
+  const [searchByID, setSearchByID] = useState<boolean>(false); // TODO make it a config object for advanced search, supporting all APISearchParams options
+
+  function handleFocus(e: React.FocusEvent<HTMLInputElement>) {
+    setIsFocused(true);
+
+    focusHandler && focusHandler();
+  }
+
+  function handleChange(e: React.ChangeEvent<HTMLInputElement>) {
+    searchHandler && searchHandler(e, searchByID ? "i" : "s");
+  }
+
   return (
     <form className="itemSearch">
       <div className="wrapper">
         <input
           className={`mainSearchField${error ? " hasError" : ""}`}
           type="text"
-          onChange={searchHandler}
-          onFocus={focusHandler}
+          onChange={handleChange}
+          onFocus={handleFocus}
+          onBlur={() => setIsFocused(false)}
           onKeyDown={(e: React.KeyboardEvent<HTMLInputElement>) =>
             e.key === "Escape" && resetHandler()
           }
-          placeholder="Search for a movie or TV show…"
+          placeholder={!isFocused ? "Search for a movie or TV show…" : ""}
           value={searchQuery}
         />
+
+        {/*{isFocused && "advancedSearch"}*/}
+
+        <>
+          <label htmlFor="searchByID">search by IMDB ID</label>
+          <input
+            type="checkbox"
+            name={"searchByID"}
+            value={String(searchByID)}
+            onChange={() => setSearchByID(enabled => !enabled)}
+          />
+        </>
 
         <button
           type="button"
