e37b0c7916e0a1ce8469d46d85cacc2cd95e8847
Alessandro Di Tecco <alessandro@ditecco.me>
2021-10-05 23:37:58 +0200

    feature/search_by_ID | Enabled style tag in MaterialIcon.tsx

--- src/components/Misc/MaterialIcon.tsx
+++ src/components/Misc/MaterialIcon.tsx
@@ -2,17 +2,23 @@
 MaterialIcon
 --------------------------------- */
 
-import React, { ReactElement } from "react";
+import React, { CSSProperties, ReactElement } from "react";
 import { addClasses } from "../../utils";
 
 interface OwnProps {
   icon?: string;
   className?: string;
+  style?: CSSProperties;
 }
 
 export default function MaterialIcon({
   icon,
   className,
+  style,
 }: OwnProps): ReactElement {
-  return <i className={"material-icons" + addClasses(className)}>{icon}</i>;
+  return (
+    <i className={"material-icons" + addClasses(className)} style={style ?? {}}>
+      {icon}
+    </i>
+  );
 }
