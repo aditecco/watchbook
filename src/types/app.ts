1a500360be0b84b8ed7dda022e901602c50f527a
Alessandro Di Tecco <alessandro@ditecco.me>
2022-01-24 00:02:40 +0100

    Added new types to app.ts

--- src/types/app.ts
+++ src/types/app.ts
@@ -10,3 +10,15 @@ export interface WatchedItem {
   type: string;
   year: string;
 }
+
+export enum RuntimeFilterLabels {
+  UP_TO_30 = "Up to 30 minutes",
+  UP_TO_60 = "Up to 60 minutes",
+  UP_TO_90 = "Up to 90 minutes",
+  UP_TO_100 = "Up to 100 minutes",
+  UP_TO_120 = "Up to 120 minutes",
+  UP_TO_180 = "Up to 180 minutes",
+  UP_TO_200 = "Up to 200 minutes",
+  UP_TO_300 = "Up to 300 minutes",
+  MORE_THAN_300 = "Over 300 minutes",
+}
