e3150257a3d507f8ac7d93a8cc51d3b51d62696f
Alessandro Di Tecco <alessandro@ditecco.me>
2022-01-28 01:12:23 +0100

    feature/tags | Created WIP Tag types

--- src/types/app.ts
+++ src/types/app.ts
@@ -1,3 +1,7 @@
+/* ---------------------------------
+Types
+--------------------------------- */
+
 import { PRIMARY_DATASET_KEY, SECONDARY_DATASET_KEY } from "../constants";
 
 export type Dataset = typeof PRIMARY_DATASET_KEY | typeof SECONDARY_DATASET_KEY;
@@ -22,3 +26,17 @@ export enum RuntimeFilterLabels {
   UP_TO_300 = "Up to 300 minutes",
   MORE_THAN_300 = "Over 300 minutes",
 }
+
+type TagType = "provider" | "status";
+
+type TagProvider = "Netflix" | "Mubi" | "NowTV" | "Amazon Prime Video" | "DVD";
+
+type TagStatus = "dropped" | "completed";
+
+export type Tag = {
+  id: string;
+  timestamp: number;
+  type: TagType;
+  value: TagProvider & TagStatus;
+  assignedTo: Record<string, boolean>;
+};
