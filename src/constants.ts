6cf1e5e53c7f0a8d76213ccac631d6e4babbdb4d
Alessandro Di Tecco <alessandro@ditecco.me>
2022-02-04 00:33:58 +0100

    Added new constants.ts

--- src/constants.ts
+++ src/constants.ts
@@ -8,7 +8,10 @@ export const OMDB_API_URL = "https://www.omdbapi.com/?apiKey=";
 export const MOCK_API = "https://jsonplaceholder.typicode.com/todos/1";
 export const PRIMARY_DATASET_KEY = "watched";
 export const SECONDARY_DATASET_KEY = "toWatch";
+export const ERROR_GENERIC_ERROR = "An error occurred.";
 export const ERROR_PRE_EXISTING_CONTENT = "Oops! You already added this item.";
+export const ERROR_PRE_EXISTING_TAG =
+  "Oops! The tag is already assigned to this item.";
 export const WARNING_VALUE_NEEDED = (item: string) =>
   `Please insert a value for this ${item}.`;
 
