0ea04ddb36b99cce52f4bfc19dd5ff2d6458c44a
Alessandro Di Tecco <alessandro@ditecco.me>
2022-02-23 00:51:50 +0100

    Added `TagContentMapper`

--- src/lib/index.tsx
+++ src/lib/index.tsx
@@ -3,7 +3,7 @@ lib
 --------------------------------- */
 
 import React from "react";
-import { OMDBitem } from "../types";
+import { OMDBitem, TagType } from "../types";
 
 export function OMDBcontentMapper(onItemClick: (arg: string) => void) {
   return function (searchItem: OMDBitem, i: number) {
@@ -19,6 +19,31 @@ export function OMDBcontentMapper(onItemClick: (arg: string) => void) {
             <span className="ItemType">{searchItem.Type}</span>
             {", "}
             <span className="ItemYear">{searchItem.Year}</span>
+          </p>
+        </div>
+      </li>
+    );
+  };
+}
+
+export function TagContentMapper(onItemClick: (arg: string) => void) {
+  return function (tag: TagType, i: number) {
+    return (
+      <li className="AutoSuggestItem wrapper" key={i}>
+        <div
+          className="AutoSuggestItemLinkTarget"
+          onClick={() => onItemClick(tag.id)}
+        >
+          <h4 className="AutoSuggestItemTitle">{tag.label}</h4>
+
+          <p className="AutoSuggestItemDesc">
+            <span className="ItemType">
+              {Object.keys(tag.assignedTo ?? {}).length} usages
+            </span>
+            {", "}
+            <span className="ItemYear">
+              {new Date(tag.timestamp)?.toLocaleDateString?.()}
+            </span>
           </p>
         </div>
       </li>
