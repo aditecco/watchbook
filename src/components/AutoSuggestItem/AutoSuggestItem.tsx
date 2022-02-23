fe84cd5d53dff01ffbd0799360f5870b0de8a8f8
Alessandro Di Tecco <alessandro@ditecco.me>
2022-02-24 00:50:39 +0100

    Moved UI logic to AutoSuggestItem.tsx

---
+++ src/components/AutoSuggestItem/AutoSuggestItem.tsx
@@ -0,0 +1,34 @@
+/* ---------------------------------
+AutoSuggestItem
+--------------------------------- */
+
+import * as React from "react";
+import { PropsWithChildren, ReactElement } from "react";
+
+type OwnProps = {
+  clickHander;
+  label: string;
+  desc_1: string;
+  desc_2: string;
+};
+
+export default function AutoSuggestItem({
+  clickHander,
+  label,
+  desc_1,
+  desc_2,
+}: PropsWithChildren<OwnProps>): ReactElement {
+  return (
+    <li className="AutoSuggestItem wrapper">
+      <div className="AutoSuggestItemLinkTarget" onClick={clickHander}>
+        <h4 className="AutoSuggestItemTitle">{label}</h4>
+
+        <p className="AutoSuggestItemDesc">
+          <span className="ItemType">{desc_1}</span>
+          {", "}
+          <span className="ItemYear">{desc_2}</span>
+        </p>
+      </div>
+    </li>
+  );
+}
--- src/lib/index.tsx
+++ src/lib/index.tsx
@@ -4,24 +4,18 @@ lib
 
 import React from "react";
 import { OMDBitem, TagType } from "../types";
+import AutoSuggestItem from "../components/AutoSuggestItem/AutoSuggestItem";
 
 export function OMDBcontentMapper(onItemClick: (arg: string) => void) {
   return function (searchItem: OMDBitem, i: number) {
     return (
-      <li className="AutoSuggestItem wrapper" key={i}>
-        <div
-          className="AutoSuggestItemLinkTarget"
-          onClick={() => onItemClick(searchItem.imdbID)}
-        >
-          <h4 className="AutoSuggestItemTitle">{searchItem.Title}</h4>
-
-          <p className="AutoSuggestItemDesc">
-            <span className="ItemType">{searchItem.Type}</span>
-            {", "}
-            <span className="ItemYear">{searchItem.Year}</span>
-          </p>
-        </div>
-      </li>
+      <AutoSuggestItem
+        key={i}
+        clickHander={() => onItemClick(searchItem.imdbID)}
+        label={searchItem.Title}
+        desc_1={searchItem.Type}
+        desc_2={searchItem.Year}
+      />
     );
   };
 }
@@ -29,24 +23,13 @@ export function OMDBcontentMapper(onItemClick: (arg: string) => void) {
 export function TagContentMapper(onItemClick: (arg: string) => void) {
   return function (tag: TagType, i: number) {
     return (
-      <li className="AutoSuggestItem wrapper" key={i}>
-        <div
-          className="AutoSuggestItemLinkTarget"
-          onClick={() => onItemClick(tag.id)}
-        >
-          <h4 className="AutoSuggestItemTitle">{tag.label}</h4>
-
-          <p className="AutoSuggestItemDesc">
-            <span className="ItemType">
-              {Object.keys(tag.assignedTo ?? {}).length} usages
-            </span>
-            {", "}
-            <span className="ItemYear">
-              {new Date(tag.timestamp)?.toLocaleDateString?.()}
-            </span>
-          </p>
-        </div>
-      </li>
+      <AutoSuggestItem
+        key={i}
+        clickHander={() => onItemClick(tag.id)}
+        label={tag.label}
+        desc_1={`${Object.keys(tag.assignedTo ?? {}).length} usages`}
+        desc_2={new Date(tag.timestamp)?.toLocaleDateString?.() ?? ""}
+      />
     );
   };
 }
